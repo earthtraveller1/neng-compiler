b54db6d395d48088750d12745a4e3fb1963b0a26
earthtraveller1 <liangyuezhao7@gmail.com>
2026-03-22 16:30:25 -0400

    Added a very basic command line parser - it just ignores everything it doesn't understand lol

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::env;
+
+struct CommandLineOptions {
+    input_file: String,
+    output_file: String,
+}
+
+impl CommandLineOptions {
+    fn get() -> CommandLineOptions {
+        let mut options = CommandLineOptions {
+            input_file: String::new(),
+            output_file: "a.bf".to_string(),
+        };
+
+        for arg in env::args() {
+            if arg.starts_with("-i=") {
+                // We can safely unwrap here as we already ensures that the prefix exists
+                options.input_file = arg.strip_prefix("-i=").unwrap().to_string();
+            } else if arg.starts_with("-o=") {
+                options.output_file = arg.strip_prefix("-o=").unwrap().to_string();
+            }
+        }
+
+        options
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli_options = CommandLineOptions::get();
+    println!("Input: {}, Output: {}", cli_options.input_file, cli_options.output_file);
 }
