995376478a84605879040d03d94c267e7cda02f9
earthtraveller1 <liangyuezhao7@gmail.com>
2026-03-22 17:02:28 -0400

    Wrote a basic tokenizer.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::env;
 
+#[cfg(test)]
+mod tests;
+
 struct CommandLineOptions {
     input_file: String,
     output_file: String,
@@ -25,7 +28,61 @@ impl CommandLineOptions {
     }
 }
 
+fn tokenize_string(string: &str) -> Vec<&str> {
+    let mut tokens = Vec::new();
+
+    // Every alphanumberical word is a token
+    // Every random character is a token
+    // idk
+
+    let mut current_token_start = 0;
+    // the token starts out as empty
+    let mut current_token = &string[current_token_start..current_token_start];
+
+    for (i, c) in string.char_indices() {
+        if c.is_alphanumeric() {
+            current_token =
+                &string[current_token_start..(current_token_start + current_token.len() + 1)];
+        } else {
+            // This implies something is in the current token already
+            if !current_token.is_empty() {
+                tokens.push(current_token);
+            }
+
+            // All alphanumberical characters are individual tokens
+            if !c.is_whitespace() {
+                tokens.push(&string[i..i+1]);
+            }
+
+            // Move it forward to the next one
+            current_token_start = i + 1;
+            current_token = &string[current_token_start..current_token_start];
+        }
+    }
+
+    tokens
+}
+
 fn main() {
     let cli_options = CommandLineOptions::get();
-    println!("Input: {}, Output: {}", cli_options.input_file, cli_options.output_file);
+    println!(
+        "Input: {}, Output: {}",
+        cli_options.input_file, cli_options.output_file
+    );
+
+    if cli_options.input_file.is_empty() {
+        eprintln!("[ERROR]: No input file specified.");
+        return;
+    }
+
+    let source_file = match std::fs::read_to_string(&cli_options.input_file) {
+        Ok(x) => x,
+        Err(e) => {
+            eprintln!("[ERROR]: Failed to read {}: {}", &cli_options.input_file, e);
+            return;
+        }
+    };
+
+    let tokens = tokenize_string(&source_file);
+    println!("Tokens: {:?}", tokens);
 }
